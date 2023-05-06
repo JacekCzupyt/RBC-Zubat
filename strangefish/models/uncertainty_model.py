f0a66398f4ffb7eb12d9113939cc14b19e49968e
JacekCzupyt <kuba.czupyt@gmail.com>
2023-05-06 18:43:35 +0200

    Uncertainty transformer tarining

--- strangefish/models/uncertainty_model.py
+++ strangefish/models/uncertainty_model.py
@@ -5,12 +5,12 @@ from tensorflow.python.keras.layers import Conv2D, Activation, Dropout, LSTM, De
 from tensorflow.python.keras.models import Model
 
 
-def uncertainty_model_1(weights_path):
+def uncertainty_model_1(weights_path=None):
     def masked_squared_error(y_true, y_pred):
         mask = tf.where(tf.not_equal(y_true, -1.0))
         return tf.reduce_mean(tf.square(tf.gather(y_true, mask) - tf.gather(y_pred, mask)))
 
-    input_dim = (8, 8, 37)
+    input_dim = (None, 8, 8, 37)
 
     dropout = 0
 
@@ -32,7 +32,7 @@ def uncertainty_model_1(weights_path):
     x = Activation('relu', name='activation_2')(x)
     x = Dropout(dropout, name='dropout_2')(x)
 
-    x = Reshape((-1, 4*4*256), name='reshape_1')(x)
+    x = Reshape((-1, 4 * 4 * 256), name='reshape_1')(x)
 
     x = LSTM(128, activation='relu', return_sequences=True,
              dropout=dropout, name='lstm_1')(x)
@@ -48,9 +48,7 @@ def uncertainty_model_1(weights_path):
 
     model.compile(loss=masked_squared_error, optimizer='adam')
 
-    model.load_weights(weights_path)
+    if weights_path is not None:
+        model.load_weights(weights_path)
 
     return model
-
-
-
