ae44d087e44de9aa45e436178830dec6087ce2b4
JacekCzupyt <kuba.czupyt@gmail.com>
2023-07-25 00:05:03 +0200

    Add recapture weights to risk-taker module

--- strangefish/zubat_strategy/risk_taker.py
+++ strangefish/zubat_strategy/risk_taker.py
@@ -17,7 +17,9 @@ def get_high_risk_moves(
         time_limit=None,
         depth=1,
         samples=1500,
-        score_config: ScoreConfig = ScoreConfig(capture_king_score=100, checkmate_score=90, into_check_score=-100, remain_in_check_penalty=-20, op_into_check_score=-40),
+        recapture_weight=5,
+        score_config: ScoreConfig = ScoreConfig(),
+        # score_config: ScoreConfig = ScoreConfig(capture_king_score=100, checkmate_score=90, into_check_score=-100, remain_in_check_penalty=-20, op_into_check_score=-40),
         rc_disable_pbar=False,
 ):
     results = {move: [] for move in moves}
@@ -25,29 +27,34 @@ def get_high_risk_moves(
     for _ in tqdm(range(samples), desc="Sampling for gambles", unit="Samples", disable=rc_disable_pbar):
         try:
             board = fast_copy_board(random.choice(boards))
-            move = random.choice(moves)
+            considered_move: chess.Move = random.choice(moves)
             for i in range(depth):
                 if i == 0:
-                    my_move = move
+                    my_move: chess.Move = considered_move
                 else:
-                    my_move = random.choice(rbc_legal_moves(board))
-                # my_taken_move = simulate_move(board, my_move)
+                    my_move: chess.Move = random.choice(rbc_legal_moves(board))
+
+                # If performing a capture move, expect the enemy to attempt to recapture
+                is_capture = board.is_capture(my_move)
+
                 board.push(my_move)
                 if board.king(board.turn) is None:
-                    results[move].append(score_config.capture_king_score + score_material(board, board.turn))
+                    results[considered_move].append(score_config.capture_king_score + score_material(board, board.turn))
                     break
 
-                opponent_move = random.choice(rbc_legal_moves(board))
+                if is_capture:
+                    # NOTE: this does not take into account en-passant (in either direction)
+                    weights = [recapture_weight if m.to_square == my_move.to_square else 1 for m in rbc_legal_moves(board)]
+                else:
+                    weights = None
 
-                # print(board)
-                # print(opponent_move)
-                # print('')
+                opponent_move = random.choices(rbc_legal_moves(board), weights=weights)[0]
 
-                score = -calculate_score(engine, board, move=opponent_move, is_op_turn=False)
+                score = -calculate_score(engine, board, move=opponent_move, is_op_turn=False, score_config=score_config)
                 # opponent_taken_move = simulate_move(board, opponent_move)
                 board.push(opponent_move)
 
-                results[move].append(score)
+                results[considered_move].append(score)
             if time_limit is not None and time() - start_time > time_limit:
                 print('Time limit for gamble sampling exceeded')
                 break
